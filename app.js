28a3a1d72d07f7b88cb8343f91a01697e31e56db
Julia Nocera <giulz42@gmail.com>
2016-02-15 23:43:47 -0800

    structures app, adds public folder, server file: 'app.js', gitignore and package.json

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var cors = require("cors");
+var bodyParser = require("body-parser");
+var app = express();
+
+var server = require('http').Server(app);
+var port = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// logs the request type such as 'GET' and the url path its calling 
+app.use(function(req, res, next) {
+	console.log(`${req.method} request for '${req.url}' - ${JSON.stringify(req.body)}`);
+	next();
+});
+
+app.use(express.static("./public"));
+
+app.use(cors());
+
+server.listen(port, function() {
+  console.log('Running on port: ', port);
+});
+
+console.log("Express app running on port: ", port);
+
+module.exports = app;
