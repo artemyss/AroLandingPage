4b6f32d46a1235de8f0017c7e8956231c2d99f84
Julia Nocera <giulz42@gmail.com>
2016-02-17 00:53:51 -0800

    Changes port configuration and adds Procfile

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ var bodyParser = require("body-parser");
 var app = express();
 
 var server = require('http').Server(app);
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8100;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // logs the request type such as 'GET' and the url path its calling 
-app.use(function(req, res, next) {
-	console.log(`${req.method} request for '${req.url}' - ${JSON.stringify(req.body)}`);
-	next();
-});
+// app.use(function(req, res, next) {
+// 	console.log(`${req.method} request for '${req.url}' - ${JSON.stringify(req.body)}`);
+// 	next();
+// });
 
 app.use(express.static("./public"));
 
