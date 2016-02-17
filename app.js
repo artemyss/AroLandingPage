9039fcca6edb44665b2b88f9e21471865d0da976
Julia Nocera <giulz42@gmail.com>
2016-02-17 01:21:08 -0800

    verbose logging

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 var express = require('express');
 var bodyparser = require('body-parser');
-// var mongoose = require('mongoose');
 var http = require('http');
 var app = express();
  
-// mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/meals-development');
 app.use(express.static(__dirname + '/public'));
  
 app.use(bodyparser.json({limit:'50mb'}));
 app.use(bodyparser.urlencoded({limit: '50mb', extended: true}));
  
-require('./routes/admin-routes')(app);
  
 var server = http.createServer(app);
  
